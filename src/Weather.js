14256009e97f72f8900aa52f3a561847219b1ffd
anniesalis <anniesalis02@gmail.com>
2024-11-10 01:32:39 +0000

    Added basic styling.

--- src/Weather.js
+++ src/Weather.js
@@ -1,88 +1,72 @@
 import React, { useState, } from 'react';
 import axios from 'axios';
 import './Weather.css';
-
-import ReactAnimatedWeather from "react-animated-weather/build/ReactAnimatedWeather";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-export default function Weather(props) {
+export default function Weather() {
   
-    let [city, setCity] = useState(null);
-    let [message, setMessage] = useState(false);
-    let [weather, setWeather] = useState({});
-  
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-      let apiKey = "4b3503b2f08a729413c4d33ef1186004";
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-      console.log(apiUrl);
-      axios.get(apiUrl).then(showWeather);
-    }
-  
-  
-    function changeCity(event) {
-      setCity(event.target.value);
-    }
+          return (
+            
+        <div className='weather'>
+          <form>
+            <div className='row'>
+              <div className='col-9'>
+            <input type='search'
+             placeholder='Enter a City'
+             required className='form-control'
+             autoFocus='on'
+             />
+             </div>
+             <div className='col-3'>
 
-  
-    function showWeather(response) {
-      setMessage(true);
-      setWeather({
-        temperature: response.data.main.temp,
-        description: response.data.weather[0].description,
-        humidity: response.data.main.humidity,
-        wind: response.data.wind.speed,
-      });
-    }
-  
-  
-    let form = (
-      
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          required
-          onChange={changeCity}
-          className="searchBox"
-        />
-        <input type="submit" value="Search" className="searchButton" />
-      </form>
-    );
-  
-    https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png
-    if (message) {
-      return (
-        <div className='container'>
-          {form}
-          <div className='row'>
-            <div className='col-sm-4 pt-3'>
-              <div className='city'>  </div>
-            <div className='description text-danger'><ReactAnimatedWeather
-              icon="CLOUDY"
-              color=""
-              size={40}
-              animate={true}
-            />{" "} {weather.description}</div>
-            <div  className='temp'> <h1><strong> {Math.round(weather.temperature)}°C </strong></h1></div>
+            <input type='submit' value='search' className='btn btn-primary w-100' />
+            
             </div>
-            <br />
-                        
+            </div>
          
-           <div className='col-sm-'> 
-                  <div className='humidity'>Humidity: {Math.round(weather.humidity)}%</div>
-            <div className='wind text-warning'>Wind: {Math.round(weather.wind)}km/h</div>
-            </div>
-            </div>
-      
+         </form>
+
+
+           <h1>Porto</h1>
+             <ul>
+                           <li>
+                Wednesday 07:00
+              </li>
+              <li>
+              Mostly Cloudy
+              </li>
+
+             </ul>
           
-        </div>
-        
-      );
-    } else {
-      setMessage(" ");
-    }
-  }
-  
+          <div className='row'>
+            <div className='col-6'> 
+            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"  alt="weather icon"/> 
+            {" "}  <span className='temperature'>7 </span>°C | ºF
+              </div>
+              
+              
+              
+              
+              <div className='col-6'>
+                <ul>
+                  <li>
+                    Precipitation: 15%
+                  </li>
+                  <li>
+                    Humidity: 72%
+                  </li>
+                  <li>
+                    Wind: 13kmh%
+                  </li>
+                </ul>
+              
+                </div>
+                
+              
+              </div>
+              </div>
+              
+              
+);
+}
